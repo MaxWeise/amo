3760a100930ec115220b8ac3a7e2197aff7c5c07
MaxWeise <fencingmax21@gmail.com>
2023-07-12 11:04:40 +0200

    chore: Clean code

--- backend/tests/tinydb_adapter_test.py
+++ backend/tests/tinydb_adapter_test.py
@@ -16,44 +16,39 @@ class TestTinyDBAdapter(unittest.TestCase):
         """Create the test environment."""
         self.test_database_file = pathlib.Path("./test_database_file.json")
         self.under_test = TinyDBAdapter(self.test_database_file)
+        self.test_objects: list[Equipment] = [
+            data_objects.Weapon("#0L01"),
+            data_objects.Weapon("#0L02"),
+            data_objects.Weapon("#0L03"),
+        ]
 
     def test_create(self) -> None:
         """Test the correct creation of resources in the database."""
-        # Setup Code
         test_object: Equipment = data_objects.Weapon("#0L01")
 
-        # Run Test
         actual = self.under_test.create(test_object)
-
-        # Assert Statements
-        self.assertTrue(actual)
         actual_contents = self.under_test._database.all()
-        self.assertEqual([test_object.to_dict()], actual_contents)
+
+        self.assertTrue(actual)
+        expected_db_contents = [test_object.to_dict()]
+        self.assertEqual(actual_contents, expected_db_contents)
 
     def test_read(self) -> None:
         """Test the correct behavior when reading the database."""
-        # Setup Code
-        test_object: Equipment = data_objects.Weapon("#0L01")
-        test_object_one: Equipment = data_objects.Weapon("#0L02")
-        test_object_two: Equipment = data_objects.Weapon("#0L03")
-        entry_list: list[Equipment] = [test_object, test_object_one, test_object_two]
-        for entry in entry_list:
+        for entry in self.test_objects:
             self.under_test._database.insert(entry.to_dict())
 
-        # Run Test
         actual: list[dict[str, Any]] = self.under_test.read()
 
-        # Assert Statements
-        expected: list[dict[str, Any]] = [entry.to_dict() for entry in entry_list]
+        expected: list[dict[str, Any]] = [
+            entry.to_dict() for entry in self.test_objects
+        ]
         self.assertEqual(actual, expected)
 
     def test_update_changeMaintenance(self) -> None:
         """Test the correct behavior when changing a resource."""
-        # Setup Code
-        test_object: Equipment = data_objects.Weapon("#0L01")
-        self.under_test._database.insert(test_object.to_dict())
-        test_object_one: Equipment = data_objects.Weapon("#0L02")
-        self.under_test._database.insert(test_object_one.to_dict())
+        for entry in self.test_objects:
+            self.under_test._database.insert(entry.to_dict())
 
         test_user: User = data_objects.User(
             "user_name", "example@email.com", "Random Password"
@@ -63,13 +58,13 @@ class TestTinyDBAdapter(unittest.TestCase):
         )
         test_maintenance: list[Maintenance] = [test_maintenance_entry]
 
-        # Run Test
+        test_object = self.test_objects[0]
+        test_object_one = self.test_objects[1]
         ret: bool = self.under_test.update(
             test_object, new_maintenance_list=test_maintenance
         )
         actual_db_contents = self.under_test._database.all()
 
-        # Assert Statements
         self.assertTrue(ret)
         test_object.add_maintenance(test_maintenance_entry)
         self.assertEqual(test_object.to_dict(), actual_db_contents[0])
@@ -77,19 +72,15 @@ class TestTinyDBAdapter(unittest.TestCase):
 
     def test_update_changeOwner(self) -> None:
         """Test the correct behavior when changing a resource."""
-        # Setup Code
-        test_object: Equipment = data_objects.Weapon("#0L01")
-        self.under_test._database.insert(test_object.to_dict())
-        test_object_one: Equipment = data_objects.Weapon("#0L02")
-        self.under_test._database.insert(test_object_one.to_dict())
+        for entry in self.test_objects:
+            self.under_test._database.insert(entry.to_dict())
 
         test_owner: Person = data_objects.Person("user_name", "example@email.com")
-
-        # Run Test
+        test_object = self.test_objects[0]
+        test_object_one = self.test_objects[1]
         ret: bool = self.under_test.update(test_object, new_owner=test_owner)
         actual_db_contents = self.under_test._database.all()
 
-        # Assert Statements
         self.assertTrue(ret)
         test_object.change_owner(test_owner)
         self.assertEqual(test_object.to_dict(), actual_db_contents[0])
@@ -97,22 +88,18 @@ class TestTinyDBAdapter(unittest.TestCase):
 
     def test_delete(self) -> None:
         """Test the correct behavior when deleting from the database."""
-        # Setup Code
-        test_object: Equipment = data_objects.Weapon("#0L01")
-        self.under_test._database.insert(test_object.to_dict())
-        test_object_one: Equipment = data_objects.Weapon("#0L02")
-        self.under_test._database.insert(test_object_one.to_dict())
-        test_object_two: Equipment = data_objects.Weapon("#0L03")
-        self.under_test._database.insert(test_object_two.to_dict())
+        for entry in self.test_objects:
+            self.under_test._database.insert(entry.to_dict())
+        test_object_one = self.test_objects[1]
 
-        # Run Test
         ret: bool = self.under_test.delete(test_object_one.identifier)
 
-        # Assert Statemens
         self.assertTrue(ret)
-
         actual_db_contents = self.under_test._database.all()
-        expected_db_contents = [test_object.to_dict(), test_object_two.to_dict()]
+        expected_db_contents = [
+            self.test_objects[0].to_dict(),
+            self.test_objects[2].to_dict(),
+        ]
         self.assertEqual(actual_db_contents, expected_db_contents)
 
     def tearDown(self) -> None:
